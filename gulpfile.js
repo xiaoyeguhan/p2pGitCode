6ed6185366064e0892352d158b325dd5d9bff260
zs <562757932@qq.com>
2018-09-13 13:21:12 +0800

    完成首页制作

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+
+let gulp=require('gulp');
+
+//引入less编译模块
+let less=require('gulp-less');
+
+//引入css压缩模块
+let cleanCss=require('gulp-clean-css');
+
+//引入重命名板块
+let rename=require('gulp-rename');
+
+//引入热刷新模块
+let livereload=require('gulp-livereload');
+
+//压缩js
+let uglify=require('gulp-uglify');
+
+//引入路径模块
+let path=require('path');
+
+
+//gulp-less  gulp-clean-css  gulp-rename   gulp-livereload 
+gulp.task('lessTask',()=>{
+    gulp.src('./src/less/*.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(cleanCss())
+        .pipe(rename(function(path){
+            path.basename+='.min';
+        }))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(livereload());
+})
+
+
+//压缩js
+gulp.task('jsTask',()=>{
+    gulp.src('./src/js/*.js')
+    .pipe(uglify())
+    .pipe(rename(function(path){
+        path.basename+='.min';
+    }))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(livereload());
+})
+
+
+//监听者
+gulp.task('default',()=>{
+    livereload.listen();
+    gulp.watch('./src/less/*.less',['lessTask']);
+    gulp.watch('./src/js/*.js',['jsTask']);
+ })
