9f9f716484bb04776a29d3120de15c18de102e8f
zs <562757932@qq.com>
2018-10-28 22:51:54 +0800

    测试

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>首页</title>
+    <title>首页测试</title>
     <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="./dist/css/index.min.css">
 </head>
